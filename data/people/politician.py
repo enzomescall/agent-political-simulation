8e914a3db05bf44fbd6c46791184734641e400f2
Enzo Mescall <emm121@duke.edu>
2026-04-08 11:40:34 -0400

    init commit

---
+++ data/people/politician.py
@@ -0,0 +1,5 @@
+from data.people.agent import Agent
+
+class Politician(Agent):
+    def __init__(self) -> None:
+        super().__init__()
---
+++ data/places/federal.py
@@ -0,0 +1,5 @@
+from place import Place
+
+class Federal(Place):
+    def __init__(self) -> None:
+        super().__init__()
